618b07d07609cedfa3d8eda59d2a46ea6a774bde
Ekdvs <ekdvsampath02@gmail.com>
2025-09-08 21:05:35 +0530

    fix

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -5,8 +5,8 @@ import { useLocation } from "react-router-dom";
 import PaymentForm from "./PaymentForm";
 
 // 🔑 load Stripe with your public key
-const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
-
+//const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
+stripePromise = loadStripe("pk_test_51MZqgKSGb0nX4mJH5Ykz3v1H3p7Ykz3v1H3p7Ykz3v1H3p7Ykz3v1H3p7Ykz3v1H3000");
 const PaymentPage = () => {
   const location = useLocation();
   const { order } = location.state || {};
