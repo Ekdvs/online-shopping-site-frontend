a76f914b041497de9a1fd44a66f38d56ce714f94
Ekdvs <ekdvsampath02@gmail.com>
2025-09-08 14:31:08 +0530

    title head

--- src/pages/user/Register.jsx
+++ src/pages/user/Register.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import Axios from "../../utils/Axios";
 import SummaryApi from "../../common/SummaryApi";
 import ToastProvider from "../../components/ToastProvider";
+import { Title } from "react-head";
 
 
 const Register = () => {
