f077b55543fd85cf43a5a0440c75f5685a38a463
Ekdvs <ekdvsampath02@gmail.com>
2025-09-09 16:18:03 +0530

    token fix

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -16,7 +16,7 @@ Axios.interceptors.request.use((config) => {
   return config;
 });
 
-Axios.interceptors.response.use(
+{/*Axios.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && error.response.status === 401) {
@@ -29,6 +29,6 @@ Axios.interceptors.response.use(
     }
     return Promise.reject(error);
   }
-);
+);*/}
 
 export default Axios;
